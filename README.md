f9f362b02dc149cd8f82cf280c6fe230abcdda4c
qwikster <ocoincs@gmail.com>
2026-06-05 03:17:40 -0400

    shitass mockup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 welcome to my cool website :3
+
+me [contact | links | 88x31s] / things / blag [serious | not | ??]
--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,26 @@
 <script>
-    import logo from "/public/favicon.png"
+    import logo from "/favicon.png"
 </script>
 
 <div class="content">
     <div class="header">
-        <hr><img src={logo} alt="logo"><hr>
+        <hr><div class="textbox">things</div><hr><img src={logo} alt="logo"><hr><div class="textbox">blag</div><hr>
     </div>
     <div class="textbox">
         <h1 class="text">welcome to my cool site :)</h1>
-        <p class="text">if you read this your cool</p>
+        <p class="text">this is a bunch of long text that i can put in the middle of the website to show the things that the text is saying like who i am and what the point of this site in and how much random shit i can type</p>
+        <div class="textbox">
+            <h1>links             (expand)</h1>
+        </div>
+        <div class="textbox">
+            <h1>88x31s             (expand)</h1>
+        </div>
+        <div class="textbox">
+            <h1>etc             (close)</h1>
+            <hr>
+            <p>ooo time zones and music player and uptime neat</p>
+        </div>
+        <p>i was going to put some scrolling icon buttons here to show cool things i use</p>
     </div>
     <div class="links">
         <a href="https://youtube.com/@qwiksteryt">youtube!</a>
@@ -39,6 +51,7 @@
         padding: 1vmin;
         margin-bottom: 2vmin;
         border: var(--border-tile) solid var(--dim);
+
     }
 
     .content {
