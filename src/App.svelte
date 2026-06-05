a3b93d60062e355ab4fe80792cff8aaf3678493b
qwikster <ocoincs@gmail.com>
2026-06-05 05:52:03 -0400

    greeting header

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,19 @@
 <script>
     import logo from "/favicon.png"
+    import flag_ca from "/ca.svg"
+
+    import Hi from "./lib/Hi.svelte";
 </script>
 
 <div class="content">
     <div class="header">
-        <h1>header goes here ill fix it later</h1>
+        <h1>coolass logo here</h1>
     </div>
     <div class="panel">
-        <div class="hi">
-            <span class="hi_hi"><b>hi,&nbsp;</b></span>
-            <span class="hi_i">i_</span>
-            <span class="hi_qwik"><b>qwik</b></span>
-            <span class="hi_hi">!</span>
-        </div>
-        <p>
-            I can't fucking take it any more. Among Us has singlehandedly ruined my life. The other day my teacher was teaching us Greek Mythology and he mentioned a pegasus and I immediately thought <i></i>'Pegasus? more like Mega Sus!!!!' and I've never wanted to kms more. I can't look at a vent without breaking down and fucking crying. I can't eat pasta without thinking 'IMPASTA??? THATS PRETTY SUS!!!!' Skit 4 by Kanye West. The lyrics ruined me. A Mongoose, or the 25th island of greece. The scientific name for pig. I can't fucking take it anymore.
+        <Hi/>
+        <p style="text-align: left;">
+            grrrr jappan 🇯🇵 is best country in teh world (sekai) !!!!🤬😡!!!👹🤬!!!!! west bAd grrrgghhhg japenis culture⛩🎎🎏 better than amrican🗽🍔👎!!! (&gt;~&lt;) vendor machine eveywhere 🗼and sakura trees are so 🌸 a e s t h e t i c 🌸 UwU if u hate it then your NOT a man of culture so shinē!!! ~hmph baka -_- 🏮
         </p>
-        <p>Please fucking end my suffering.</p>
     </div>
 </div>
 
@@ -26,6 +23,7 @@
         margin-bottom: 30vh;
         margin-left: 20px;
         margin-right: 20px;
+        margin-top: 20px;
     }
 
     .header {
@@ -50,20 +48,6 @@
         padding: 2px;
         line-height: 1.3;
     }
-
-    .hi {
-        display: flex;
-        color: var(--col-body);
-        font-family: var(--ff-body), monospace;
-        font-size: var(--font-header);
-        margin: 0px;
-        padding: 2px;
-        line-height: 1.3;
-    }
-
-    .hi_hi { color: var(--col-body); }
-    .hi_i { color: var(--col-dim); }
-    .hi_qwik { color: var(--accent); }
 
     h1 {
         color: var(--col-header);
---
+++ src/lib/Hi.svelte
@@ -0,0 +1,81 @@
+<script>
+
+</script>
+
+<div class="hi">
+    <div class="hi-left">
+        <span class="hi_l">&nbsp;</span>
+        <span class="hi_hi"><b>hi,</b></span>
+        <span class="hi_hi">i'm</span>
+        <span class="hi_qwik"><b>qwik</b></span>
+        <span class="hi_hi">!</span>
+    </div>
+    <div class="hi-right">
+        <!-- <img src="ca.svg" alt="Canada!" width="22px"> -->
+        <span class="flag-ca"></span>
+        <span class="flag-at">@</span>
+        <span class="flag-timezone">EST</span>
+    </div>
+</div>
+
+<style>
+    .hi {
+        display: flex;
+        justify-content: space-between;
+        color: var(--col-body);
+        font-family: var(--ff-body), monospace;
+        font-size: var(--font-header);
+        margin: 0px;
+        padding: 2px;
+        line-height: 1.3;
+    }
+
+    .hi-left {
+        display: flex;
+        align-items: center;
+    }
+
+    .hi-right {
+        display: flex;
+        align-items: center;
+        border: 1px solid var(--col-bg2);
+        border-radius: var(--border-panel);
+        background-color: var(--black);
+        padding: 0px 4px;
+        padding-left: 6px;
+    }
+
+    .hi_l {
+        color: var(--accent);
+    }
+
+    .hi_hi {
+        color: var(--col-header);
+        margin-right: 8px;
+    }
+
+    .hi_qwik {
+        color: var(--accent);
+        border: 1px solid var(--col-bg2);
+        border-radius: var(--border-panel);
+        background-color: var(--black);
+        padding: 0px 4px;
+    }
+
+    .flag-ca {
+        color: #D80621;
+        font-size: var(--font-header);
+    }
+
+    .flag-at {
+        color: var(--col-dim);
+        font-size: var(--font-tiny);
+        padding: 0px 8px;
+    }
+
+    .flag-timezone {
+        color: var(--col-header);
+        font-size: var(--font-text);
+        font-weight: bold;
+    }
+</style>
